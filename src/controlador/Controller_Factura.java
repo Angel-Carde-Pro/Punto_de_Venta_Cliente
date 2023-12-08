0318c52f0ac1a367f3ff5c8787c2b180b6062047
AngelInsta <angel.cardenas.est@tecazuay.edu.ec>
2023-12-07 23:50:22 -0500

    xd

--- src/controlador/Controller_Factura.java
+++ src/controlador/Controller_Factura.java
@@ -1,8 +1,6 @@
 package controlador;
 
-import vistas.Factura_View;
-import vistas.Login_View;
-import vistas.RFacturas_view;
+import vistas.*;
 
 /**
  *
@@ -20,9 +18,9 @@ public class Controller_Factura {
         facturaview.getBtnCrear().addMouseListener(new java.awt.event.MouseAdapter() {
             @Override
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                RFacturas_view rl = new RFacturas_view();
-                Controller_RFactura cr1 = new Controller_RFactura(rl);
-                cr1.iniciarcontrol();
+                Productos_Dialog rl = new Productos_Dialog();
+                Controller_Add_Prod cr1 = new Controller_Add_Prod(rl);
+                cr1.IniciarControl();
             }
         });
     }
